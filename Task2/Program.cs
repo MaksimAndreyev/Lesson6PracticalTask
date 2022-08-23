bbe296060c229c16b6de3d2b0f2c5012a2705404
maximandreev <maxands@yandex.ru>
2022-08-23 15:41:30 +0300

    Task 2 completed

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите b1:");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k1:");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2:");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2:");
+double k2 = Convert.ToDouble(Console.ReadLine());
+if (k1 == k2)
+{
+    Console.WriteLine("Прямые параллельны");
+}
+else
+{
+    double x = (b2-b1) / (k1-k2);
+    double y = x*k1 + b1;
+    Console.WriteLine($"Точка пересечения прямых: ({x};{y})");
+}
