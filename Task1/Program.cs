2f97a447c3897d9cb088b11938d17ef3a9aef128
maximandreev <maxands@yandex.ru>
2022-08-23 15:29:11 +0300

    Task 1 completed

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿string input = Console.ReadLine();
+int count = 0;
+string number = "";
+int convertedNumber = 0;
+for (int i=0; i<input.Length; i++)
+{
+    if (input[i] == ',')
+    {
+        convertedNumber = Convert.ToInt32(number);
+        if (convertedNumber > 0)
+        {
+            count += 1;
+        }
+        number = "";
+        continue;
+    }
+    number += input[i];
+}
+Console.WriteLine(count);
